4df21f3821567c6d7b1d595852b8c97012f27eba
Minlyy <minsong223@gmail.com>
2023-03-25 17:48:27 -0400

    components

--- src/App.js
+++ src/App.js
@@ -1,22 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+
+const books = [{title : "The Giver", author : "Lois Lowry", isbn : "0544336267", available : true},
+{title : "The Odyssey", author : "Homer", isbn : "9780140268867", available : true},
+{title : "Animal Farm", author : "George Orwell", isbn : "9780451526342", available : false},
+{title : "To Kill a Mockingbird", author : "Harper Lee", isbn : "0446310786", available : true},]
+
+function Bookshelf() {
+  const booklist = books.map(book =>
+    <tr>
+      <td>{book.title}</td>
+      <td>{book.author}</td>
+      <td>{book.isbn}</td>
+      <td style={{
+        backgroundColor: book.available ? 'green' : 'red'
+      }}><BuyButton></BuyButton></td>
+    </tr>
+    );
+  return (
+    <table className="books">
+      <tr>
+        <th>Title</th>    
+        <th>Author</th>
+        <th>ISBN</th>
+        <th>Available</th>
+      </tr>
+      {booklist}
+    </table>
+  )
+}
+
+function BuyButton() {
+  return (
+    <button>Buy</button>
+  )
+}
+
+function Navbar() {
+  return (
+    <nav>
+      <p className='nav_p'>Books</p>
+      <ul className='nav_link'>
+        <li>Home</li>
+      </ul>
+    </nav>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <Navbar></Navbar>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React with Minsong
-        </a>
+        <Bookshelf></Bookshelf>
       </header>
     </div>
   );
